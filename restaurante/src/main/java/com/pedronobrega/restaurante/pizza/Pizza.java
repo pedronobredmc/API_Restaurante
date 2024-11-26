1b79de62e210d07f92c038ebc45f5b84f342157f
pedronobredmc <pedronobrega@alu.ufc.br>
2024-11-26 12:01:43 -0300

    Adding construct, getters and setters to class pizza

--- restaurante/src/main/java/com/pedronobrega/restaurante/pizza/Pizza.java
+++ restaurante/src/main/java/com/pedronobrega/restaurante/pizza/Pizza.java
@@ -1,10 +1,32 @@
 package com.pedronobrega.restaurante.pizza;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.EnumType;
+
+@Entity
+@Table(name = "pizzas")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Pizza {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    private Sabor sabor;
-    private Tamanho tamanho;
     private Double preco;
     private boolean disponivel;
+
+    @Enumerated(EnumType.STRING)
+    private Sabor sabor;
+    @Enumerated(EnumType.STRING)
+    private Tamanho tamanho;
 }
+
